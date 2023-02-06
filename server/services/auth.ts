c2d5ddc36243964bc9941757d81f6f263d2d8dee
Fabien BERNARD <fabien0102@gmail.com>
2023-02-06 17:00:43 +0100

    Extend auth service to be used in getServerProps

--- server/services/auth.ts
+++ server/services/auth.ts
@@ -1,8 +1,8 @@
 import { clerkClient, getAuth } from "@clerk/nextjs/server";
-import { NextApiRequest } from "next";
+import { GetServerSidePropsContext, NextApiRequest } from "next";
 
 export const auth = {
-  async getUser(req: NextApiRequest) {
+  async getUser(req: NextApiRequest | GetServerSidePropsContext["req"]) {
     // get userId from request
     const { userId } = getAuth(req);
     // get full user object
