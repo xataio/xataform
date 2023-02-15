015912ea6988a4698c2b26cf1d085ef60e6e553b
Fabien BERNARD <fabien0102@gmail.com>
2023-02-15 12:36:38 +0100

    Fix stale cache

--- hooks/useReorderQuestions.tsx
+++ hooks/useReorderQuestions.tsx
@@ -10,12 +10,20 @@ export function useReorderQuestions() {
         { formId },
         reorderedQuestions.map((i, order) => ({ ...i, order }))
       );
+      reorderedQuestions.forEach((q) =>
+        utils.question.get.setData({ questionId: q.id }, (prev) =>
+          prev ? { ...prev, order: q.order } : undefined
+        )
+      );
     },
     onError(err) {
       toast.error(err.message);
     },
     onSettled(res) {
       utils.form.summary.invalidate(res ? { formId: res.formId } : undefined);
+      res?.questions.forEach(({ id }) =>
+        utils.question.get.invalidate({ questionId: id })
+      );
     },
   });
 
